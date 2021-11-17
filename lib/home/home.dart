d45ccd533466737ba274c8b5a415fd91c623f106
Andrew <yanovi4.a@gmail.com>
2021-11-17 20:54:20 +0100

    initial commit

---
+++ lib/home/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/about'),
+          child: Text(
+            'about',
+            style: Theme.of(context).textTheme.button,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/bottom_nav.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
